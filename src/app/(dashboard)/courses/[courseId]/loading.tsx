8241646616c67a943b868c27768e3804a3cbc760
nekorytaylor666 <nekorytaylor2@gmail.com>
2024-01-12 12:39:50 +0600

    fixed loading

--- src/app/(dashboard)/courses/[courseId]/loading.tsx
+++ src/app/(dashboard)/courses/[courseId]/loading.tsx
@@ -5,8 +5,8 @@ export default function Loading() {
   return (
     <div className="container max-w-screen-xl mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
-        <Skeleton className="w-full h-[300px] mt-8 col-span-2 " />
-        <Skeleton className="w-full h-[1000px] mt-8 col-span-3" />
+        <Skeleton className="w-full h-[575px] mt-8 col-span-2 " />
+        <Skeleton className="w-full h-[1500px] mt-8 col-span-3" />
       </div>
     </div>
   );
