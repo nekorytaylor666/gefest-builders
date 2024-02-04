c3c527a832af3154c832db1fec761765d4ca1817
nekorytaylor666 <nekorytaylor2@gmail.com>
2024-02-04 13:23:05 +0800

    Improve TRPC context and logging
    Enhanced TRPC provider and server client code to increase functionality
    & error handling. The TRPC provider now uses `loggerLink` for better
    logging in development and error cases. It also includes
    `unstable_httpBatchStreamLink` that dynamically sets headers, improving
    the network layer's adaptability. Moreover, the base URL calculation has
    been centralized to handle different environments seamlessly.
    
    On the server side, `createServerComponentClient` from supabase auth
    helpers is now used to include both cookies and headers in the TRPC
    context, ensuring that the TRPC API can work with the correct session
    information and source headers.
    
    These changes pave the way for more robust error tracking and a more
    reliable authentication flow across environments.

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -1,26 +1,51 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/client";
+import {
+  httpBatchLink,
+  loggerLink,
+  unstable_httpBatchStreamLink,
+} from "@trpc/client";
 import React, { useState } from "react";
 import superjson from "superjson";
 
 import { trpc } from "./client";
 
-export default function Provider({ children }: { children: React.ReactNode }) {
+export default function Provider(props: {
+  children: React.ReactNode;
+  headersPromise: Promise<Headers>;
+}) {
   const [queryClient] = useState(() => new QueryClient({}));
   const [trpcClient] = useState(() =>
     trpc.createClient({
       transformer: superjson,
       links: [
-        httpBatchLink({
-          url: process.env.NEXT_PUBLIC_BASE_URL + "/api/trpc",
+        loggerLink({
+          enabled: (op) =>
+            process.env.NODE_ENV === "development" ||
+            (op.direction === "down" && op.result instanceof Error),
+        }),
+        unstable_httpBatchStreamLink({
+          url: getBaseUrl() + "/api/trpc",
+          async headers() {
+            const headers = new Map(await props.headersPromise);
+            headers.set("x-trpc-source", "nextjs-react");
+            return Object.fromEntries(headers);
+          },
         }),
       ],
     })
   );
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        {props.children}
+      </QueryClientProvider>
     </trpc.Provider>
   );
+}
+
+function getBaseUrl() {
+  if (typeof window !== "undefined") return window.location.origin;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  return `http://localhost:${process.env.PORT ?? 3000}`;
 }
